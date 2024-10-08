1e51779d4175118e27ea46158ee5804da402fdf7
amnashafiiq1659 <amnashafiq1659@gmail.com>
2024-10-08 20:43:04 +0500

    Assignment done!

---
+++ app/About/page.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from "react";
+
+const AboutPage = () => {
+  return <Fragment><h1 style={{color:"deeppink"}}>This is About page.</h1></Fragment>
+}
+
+export default AboutPage
---
+++ app/Service/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServicePage() {
+  return (
+    <div>
+      <h1 style={{color:"green"}}>This is Service page.</h1>
+    </div>
+  )
+}
